5d667f5a96a1c9effb78d66589717b7451564ad7
noorfarooqy <noorfarooqy@gmail.com>
2021-01-09 16:49:53 +0400

    set collation in env

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -15,11 +15,11 @@ class CreateFailedJobsTable extends Migration
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid',10000)->unique();
-            $table->text('connection',1000000);
-            $table->text('queue',1000000);
-            $table->longText('payload',1000000000);
-            $table->longText('exception',1000000000);
+            $table->string('uuid')->unique();
+            $table->text('connection');
+            $table->text('queue');
+            $table->longText('payload');
+            $table->longText('exception');
             $table->timestamp('failed_at')->useCurrent();
         });
     }
